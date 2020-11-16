7fc2076fc26b8e351eae0a89b7a5e2847a53375d
OtavioLhamas <otavio.m.lhamas@gmail.com>
2020-11-16 19:46:35 -0300

    - Migrate from Mocha and Chai to Jest for unit testing;

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,24 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import { createLocalVue, mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import Vuetify from 'vuetify'
 
 describe('HelloWorld.vue', () => {
+  const localVue = createLocalVue()
+  let vuetify: Vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+    const wrapper = mount(HelloWorld, {
+      localVue,
+      vuetify,
+      propsData: {
+        msg
+      }
     })
-    expect(wrapper.text()).to.include(msg)
+    expect(wrapper.text()).toMatch(msg)
   })
 })
---
+++ tests/unit/setup.ts
@@ -0,0 +1,5 @@
+import vuetify from '@/plugins/vuetify'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
